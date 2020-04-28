d15bde04c40a27144d441b3efb5ff2f2546729b8
smrutiranjan143 <smrutisahoo95@gmail.com>
2020-04-29 01:51:40 +0530

    convert jsp to angular jsp page

--- practice/src/main/resources/static/js/RegisterController.js
+++ practice/src/main/resources/static/js/RegisterController.js
@@ -1,13 +1,13 @@
 var app = angular.module('app', []);
 
 app.controller('registerController', function($scope, $http, $location) {
-	  $scope.submitClick = function(){
-	    var url = $location.absUrl() + "getallcustomer";
+	$scope.submitClick = function(){
+	   var url = $location.absUrl() + "getallcustomer";
 	    
-	    $http.get(url).then(function (response) {
-	      $scope.response = response.data
-	    }, function error(response) {
-	      $scope.postResultMessage = "Error with status: " +  response.statusText;
-	    });
-	  }
-	});
+	   $http.get(url).then(function (response) {
+	     $scope.response = response.data
+	   }, function error(response) {
+	     $scope.postResultMessage = "Error with status: " +  response.statusText;
+	   });
+	}
+});
