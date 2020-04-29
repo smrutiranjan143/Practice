4192aa2a1e3fe0da966ac9fa7b9065e8d466aa7c
dillipkumarjena <djena543@gmail.com>
2020-04-29 17:25:33 +0530

    Data will save in Database Successfully. Changed the registercontroler.js

--- practice/src/main/resources/static/js/RegisterController.js
+++ practice/src/main/resources/static/js/RegisterController.js
@@ -2,11 +2,12 @@ var app = angular.module('app', []);
 
 app.controller('registerController', function($scope, $http, $location) {
 	$scope.submitClick = function(){
-	   var url = $location.absUrl() + "getallcustomer";
+	   var url = $location.absUrl() + "register";
 	    
-	   $http.get(url).then(function (response) {
-	     $scope.response = response.data
-	   }, function error(response) {
+	   $http.get(url).success(function (data) {
+	     $scope.Data = data;
+	   }).
+	   error(function(data)) {
 	     $scope.postResultMessage = "Error with status: " +  response.statusText;
 	   });
 	}
